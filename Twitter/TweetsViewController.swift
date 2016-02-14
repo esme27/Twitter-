bbe27788e25e370584dd5db079845f7deebdff99
Esme <Esme@Esmes-MacBook-Pro.local>
2016-02-14 12:21:41 -0800

    adding TweetsVewcontroller

--- Twitter/TweetsViewController.swift
+++ Twitter/TweetsViewController.swift
@@ -7,20 +7,94 @@
 //
 
 import UIKit
+import AFNetworking
 
-class TweetsViewController: UIViewController {
+class TweetsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var refreshControl: UIRefreshControl!
+    let delay = 3.0 * Double(NSEC_PER_SEC)
+    var tweets: [Tweet]?
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
+        
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        refreshControl = UIRefreshControl()
+        tableView.addSubview(refreshControl)
+        
+        refreshControl.addTarget(self, action: "onRefresh", forControlEvents: UIControlEvents.ValueChanged)
+        
+        
+        TwitterClient.sharedInstance.homeTimelineWithParams(nil , completion: { (tweets, error) -> () in
+            self.tweets = tweets
+            //can reload here too
+        })
+}
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    
+    func delay(delay:Double, closure:() -> ()) {
+        dispatch_after(
+            dispatch_time(
+                DISPATCH_TIME_NOW,
+                Int64(delay * Double(NSEC_PER_SEC))
+            ),
+            dispatch_get_main_queue(), closure)
+    }
+    
+    func onRefresh() {
+        delay(1, closure: {
+            self.refreshControl.endRefreshing()
+        })
+    }
+    
+    
+    
+    @IBAction func onLogout(sender: AnyObject) {
+         User.currentUser!.logout();
+        
+        
+        
+        
+    }
+    
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("ActualTweetTableViewCell", forIndexPath: indexPath) as! ActualTweetTableViewCell
+        
+        
+        
+        
+        let url = NSURL(string: tweets![indexPath.row].user!.profileImageUrl!);
+        cell.profilePicture.setImageWithURL(url!)
+        cell.userName.text = tweets![indexPath.row].user!.name!
+        cell.twitterName.text = "@" + (tweets![indexPath.row].user?.screenname!)!
+        cell.actualTweet.text = tweets![indexPath.row].text!
+        cell.timeStamp.text = tweets![indexPath.row].createdAtString!
+        
+        
+        return cell
+        
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let tweets = self.tweets {
+            return tweets.count
+            
+        }
+        return 0
+    }
 
     /*
     // MARK: - Navigation
--- Twitter/ViewController.swift
+++ Twitter/ViewController.swift
@@ -7,12 +7,23 @@
 //
 
 import UIKit
+import BDBOAuth1Manager
+import AFNetworking
+
+
+
+
 
 class ViewController: UIViewController {
-
+    
+    //var user: User?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+       
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +31,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onLogin(sender: AnyObject) {
+        
+        
+             
+        
+        TwitterClient.sharedInstance.loginWithCompletion() {
+            (user: User?, error: NSError?) in
+            if let _ = user {
+                //perform segue
+                self.performSegueWithIdentifier("loginSegue", sender: self)
+            } else {
+                //handle login error
+                print("login error")
+            }
+        }
+        
+        
+       
+   }
 
 }
-
